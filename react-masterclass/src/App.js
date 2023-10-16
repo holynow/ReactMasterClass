55b0500fff1ab7a9ec9941232d14e7b984d0c3ba
sindy970 <sindy970@dooribun.com>
2023-10-16 14:11:07 +0900

    'styled-components'

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,24 +1,51 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const rotateAni = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0;
+  }
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Emoji = styled.span`
+  font-size: 60px;
+  transition: font-size 0.4s ease-in-out;
+`;
+
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${rotateAni} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 100px;
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="whitesmoke" />
-    </Father>
+    <Wrapper>
+      <Box>
+        <Emoji>😍</Emoji>
+      </Box>
+      <Emoji>🍙</Emoji>
+    </Wrapper>
   );
 }
 
