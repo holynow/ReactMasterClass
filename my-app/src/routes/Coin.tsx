d749ad3ca545fbcabf05084b5de9d69c19df4a60
sindy970 <sindy970@dooribun.com>
2023-10-31 17:55:34 +0900

    코인상세페이지

--- my-app/src/routes/Coin.tsx
+++ my-app/src/routes/Coin.tsx
@@ -1,12 +1,48 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.span`
+  display: block;
+  text-align: center;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const location = useLocation();
+  console.log(location);
+
+  return (
+    <Container>
+      <Header>
+        <Title>코인 {coinId}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 
 export default Coin;
